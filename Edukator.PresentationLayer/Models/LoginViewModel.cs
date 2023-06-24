f752c1e66fc0bcbe8f0ae6bd4debedd333c06383
Enes34 <ertaseness34@gmail.com>
2023-06-24 22:47:55 +0300

    finished

--- Edukator.PresentationLayer/Models/LoginViewModel.cs
+++ Edukator.PresentationLayer/Models/LoginViewModel.cs
@@ -7,7 +7,7 @@ namespace Edukator.PresentationLayer.Models
 {
     public class LoginViewModel
     {
-        public string Username { get; set; }
+        public string Username { get; set; }  
         public string Password { get; set; }
     }
 }
